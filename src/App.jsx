a9345ecb195513996c104e94475d2fa19a545cd1
sadmanryanriad <sadmanryanriad@gmail.com>
2023-09-15 22:05:49 +0600

    credit_hour added

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,24 @@ import Courses from './components/Courses'
 function App() {
 
     const [cart,setCart] = useState([]);
+    const [totalHour,setTotalHour] = useState(0);
 
     const handleCartButton = (course) =>{
+
+      const isExist = cart.find(data=> data===course);
+
+      let num = course.credit_hour;
+
+      if(isExist) return alert('already added');
+
+      else{
+        cart.forEach(item => {
+          num += item.credit_hour;
+        });
+      }
+      if(num>20) return alert('credit limit exceeded');
+      setTotalHour(num);
+
       setCart([...cart, course]);
     }
 
@@ -20,7 +36,7 @@ function App() {
 
     <div className='flex flex-col md:flex-row md:justify-between max-w-[1400px] mx-auto'>
     <Courses handleCartButton={handleCartButton}></Courses>
-    <Cart cart={cart}></Cart>
+    <Cart cart={cart} totalHour={totalHour}></Cart>
     </div>
     </>
   )
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,10 @@
 import PropTypes from 'prop-types'; // ES6
 import ListItem from './ListItem';
-const Cart = ({ cart }) => {
+
+
+
+const Cart = ({ cart,totalHour }) => {
+
     return (
         <div className="md:w-1/4 w-80 h-max rounded-xl shadow-xl p-6 mx-auto md:mx-0">
             <h2 className="text-[#2F80ED] text-xl font-bold py-6 border-b-2">Credit Hour Remaining 7 hr</h2>
@@ -10,7 +14,7 @@ const Cart = ({ cart }) => {
                     cart.map((item, key) => <ListItem key={key} cartTitle={item.title}></ListItem>)
                 }
             </ol>
-            <h3 className="text-base font-semibold py-4 my-6 border-t-2 border-b-2">Total Credit Hour : 13</h3>
+            <h3 className="text-base font-semibold py-4 my-6 border-t-2 border-b-2">Total Credit Hour : {totalHour}</h3>
             <h3 className="text-base font-semibold">Total Price : 48000 USD</h3>
 
         </div>
@@ -18,7 +22,8 @@ const Cart = ({ cart }) => {
 };
 
 Cart.propTypes = {
-    cart: PropTypes.array.isRequired
+    cart: PropTypes.array.isRequired,
+    totalHour: PropTypes.number
 }
 
 export default Cart;
